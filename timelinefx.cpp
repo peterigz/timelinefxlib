2d4cfb76845707f169cfb8d5e40a2bae08df3b5e
Peter Rigby <contact@peterrigby.me.uk>
2026-05-08 15:57:01 +0100

    Set max instensity to 20

--- timelinefx.cpp
+++ timelinefx.cpp
@@ -7399,6 +7399,7 @@ tfx_vec2_t tfx__get_max_graph_values(tfx_graph_preset preset) {
 	case tfx_graph_preset::tfxColorPreset:
 		return { 1.f, 255.f };
 	case tfx_graph_preset::tfxIntensityOvertimePreset:
+		return { 1.f, 20.f };
 	case tfx_graph_preset::tfxGradientMapperOvertimePreset:
 		return { 1.f, 10.f };
 	case tfx_graph_preset::tfxPathDirectionOvertimePreset:
