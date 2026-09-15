cd5ba43e5b5a408e355101e0dbf47ca952447154
Peter Rigby <contact@peterrigby.me.uk>
2026-09-15 16:21:20 +0100

    Update timelinefx.cpp

--- timelinefx.cpp
+++ timelinefx.cpp
@@ -12812,12 +12812,12 @@ void tfx_SetEffectTemplateWarmupTime(tfx_effect_template effect_template, float 
 	effect_template->effect->warmup_time = millisecs;
 }
 
-tfxAPI bool tfx_IsFiniteEffect(tfx_effect_template effect_template) {
+bool tfx_IsFiniteEffect(tfx_effect_template effect_template) {
 	TFX_ASSERT_HANDLE(effect_template);
 	return tfx__is_finite_effect(effect_template->effect);
 }
 
-tfxAPI float tfx_GetEffectLifetime(tfx_effect_descriptor effect_template) {
+float tfx_GetEffectLifetime(tfx_effect_descriptor effect_template) {
 	TFX_ASSERT_HANDLE(effect_template);
 	return tfx__get_effect_lifetime(effect_template, 16.666666667f);
 }
